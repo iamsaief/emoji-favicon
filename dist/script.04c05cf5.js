2426ba58071904f68c00aa2b79ba142fc17203a4
iamsaief <saiefalemon@gmail.com>
2022-09-12 19:45:42 +0600

    added vendor css on html

--- dist/script.04c05cf5.js
+++ dist/script.04c05cf5.js
@@ -28312,7 +28312,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52879" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64818" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
